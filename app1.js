ec16c7ef0c117ef32418ad4b7e427ad2b9df7caf
Gevorg <mirzoyan.gevorg.23@gmail.com>
2022-09-25 17:49:22 +0400

    ciklner

---
+++ app1.js
@@ -0,0 +1,24 @@
+// let arr = ['a' , 'b' , 'c']
+// for (let i = 0 ; i < arr.length ; i++ ) {
+//     console.log(arr[i])
+// }
+
+
+// let objectNum = {
+//     name: 'gevorg',
+//     age: 26,
+//     lang: ['en' , 'ru' , 'arm']
+// }
+
+// for ( let prop in objectNum ) {
+//     console.log(objectNum[prop])
+// }
+
+let arr1 = ['Mazda', 'BMW' , 'Mercedes']
+// for (let cars of arr1) {
+//     console.log(cars)
+// }
+
+arr1.forEach((value, key)=>{
+    console.log(value, key)
+}) 
